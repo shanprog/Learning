ca87e7feec91cb83255fda8df609da98f60de2d7
shan <shan.prog@gmail.com>
2015-03-04 11:09:54 +0900

    Глава 3 Полностью завершена

--- src/ch_03/home/lst_27_PointInTriangle.java
+++ src/ch_03/home/lst_27_PointInTriangle.java
@@ -11,9 +11,15 @@ public class lst_27_PointInTriangle {
         double x3 = input.nextDouble();
         double y3 = input.nextDouble();
 
-        double x1 = 0, y1 = 100, x2 = 200, y2 = 0, x4=x3, y4=0;
+        double x1 = 0, y1 = 100, x2 = 200;
 
 
+        double y = (y1 * x1 + x2 * y1 - y1 * x3) / x2;
+
+        if (x3 > 0 && x3 < x2 && y3 < y)
+            System.out.println("The point is in the triangle");
+        else
+            System.out.println("The point is not in the triangle");
 
     }
 }
---
+++ src/ch_03/home/lst_28_TwoRect.java
@@ -0,0 +1,34 @@
+package ch_03.home;
+
+import java.util.Scanner;
+
+public class lst_28_TwoRect {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter r1's center x-, y-coordinates, width, and height: ");
+        double x1 = input.nextDouble();
+        double y1 = input.nextDouble();
+        double width1 = input.nextDouble();
+        double height1 = input.nextDouble();
+
+        System.out.print("Enter r2's center x-, y-coordinates, width, and height: ");
+        double x2 = input.nextDouble();
+        double y2 = input.nextDouble();
+        double width2 = input.nextDouble();
+        double height2 = input.nextDouble();
+
+        double lenX = Math.abs(x1 - x2);
+        double lenY = Math.abs(y1 - y2);
+
+
+        if (lenX <= Math.abs(width1 - width2) && lenY <= Math.abs(height1 - height2)) {
+            System.out.println("r2 is inside r1");
+        }
+        else if (lenX > width1 + width2 && lenY > height1 + height2) {
+            System.out.println("r2 does not overlap r1");
+        }
+        else
+            System.out.println("r2 overlaps r1");
+    }
+}
