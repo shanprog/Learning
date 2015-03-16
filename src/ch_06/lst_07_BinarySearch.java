4d5628d9e94aedb98f19a92e3a0f869ecce213df
ShAN <shan.prog@gmail.com>
2015-03-16 16:50:25 +0200

    Глава 6.

---
+++ src/ch_06/lst_07_BinarySearch.java
@@ -0,0 +1,31 @@
+package ch_06;
+
+public class lst_07_BinarySearch {
+    public static void main(String[] args) {
+        int[] list = {2, 4, 7, 10, 11, 45, 50, 59, 60, 66, 69, 70, 79};
+        System.out.println(binarySearch(list, 2));
+        System.out.println(binarySearch(list, 11));
+        System.out.println(binarySearch(list, 12));
+        System.out.println(binarySearch(list, 1));
+        System.out.println(binarySearch(list, 3));
+    }
+
+    public static int binarySearch(int[] list, int key) {
+        int low = 0;
+        int high = list.length - 1;
+
+        while (high >= low) {
+            int mid = (low + high) / 2;
+
+            if (key < list[mid])
+                high = mid - 1;
+            else if (key == list[mid])
+                return mid;
+            else
+                low = mid + 1;
+
+        }
+
+        return -low - 1;
+    }
+}
