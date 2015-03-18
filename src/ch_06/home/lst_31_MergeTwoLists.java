a421f06ea69019b3e34976eccdda2bc852e77876
ShAN <shan.prog@gmail.com>
2015-03-18 15:16:02 +0900

    Глава 6. ДЗ Задания 31, 32

--- src/ch_06/home/lst_31_MergeTwoLists.java
+++ src/ch_06/home/lst_31_MergeTwoLists.java
@@ -6,32 +6,54 @@ public class lst_31_MergeTwoLists {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
+
         System.out.print("Enter list1: ");
         int[] list1 = new int[input.nextInt()];
 
         for (int i = 0; i < list1.length; i++)
             list1[i] = input.nextInt();
 
+
         System.out.print("Enter list2: ");
         int[] list2 = new int[input.nextInt()];
 
-        for (int i = 0; i < list1.length; i++)
-            list1[i] = input.nextInt();
+        for (int i = 0; i < list2.length; i++)
+            list2[i] = input.nextInt();
 
 
+        int[] list3 = merge(list1, list2);
 
-//        int[] list3 = merge(list1, list2);
-//        System.out.print("The merged list is ");
-//
-//        for (int aList3 : list3)
-//            System.out.print(aList3 + " ");
+        for (int i : list3)
+            System.out.print(i + " ");
 
-        System.out.println();
     }
 
-//    public static int[] merge(int[] list1, int[] list2) {
-//
-//
-//
-//    }
+    public static int[] merge(int[] list1, int[] list2) {
+
+        int[] list = new int[list1.length + list2.length];
+        int counter = 0;
+
+        for (int i = 0, j = 0; i < list1.length || j < list2.length; ) {
+
+            if (i == list1.length) {
+                list[counter] = list2[j];
+                j++;
+            } else if (j == list2.length) {
+                list[counter] = list1[i];
+                i++;
+            } else {
+                if (list1[i] <= list2[j]) {
+                    list[counter] = list1[i];
+                    i++;
+                } else {
+                    list[counter] = list2[j];
+                    j++;
+                }
+            }
+
+            counter++;
+        }
+
+        return list;
+    }
 }
