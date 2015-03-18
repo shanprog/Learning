af6aa7953b4448aeb84bbae4563e34c19ed447f4
ShAN <shan.prog@gmail.com>
2015-03-18 16:34:46 +0900

    Глава 7.

---
+++ src/ch_07/lst_01_PassTwoDimensionalArray.java
@@ -0,0 +1,36 @@
+package ch_07;
+
+import java.util.Scanner;
+
+public class lst_01_PassTwoDimensionalArray {
+    public static void main(String[] args) {
+        int[][] m = getArray(); // Get an array
+
+        System.out.println("\nSum of all elements is " + sum(m));
+    }
+
+    public static int[][] getArray() {
+        Scanner input = new Scanner(System.in);
+
+        int[][] m = new int[3][4];
+        System.out.println("Enter " + m.length + " rows and "
+                + m[0].length + " columns: ");
+        for (int i = 0; i < m.length; i++)
+            for (int j = 0; j < m[i].length; j++)
+                m[i][j] = input.nextInt();
+
+
+        return m;
+    }
+
+    public static int sum(int[][] m) {
+        int total = 0;
+
+        for (int[] aM : m) {
+            for (int anAM : aM) {
+                total += anAM;
+            }
+        }
+        return total;
+    }
+}
