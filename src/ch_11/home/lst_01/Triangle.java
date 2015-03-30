d7b60e356c2707d7bd209854ee46d95b79260e75
ShAN <shan.prog@gmail.com>
2015-03-30 16:32:18 +0900

    Глава 11. ДЗ. Задания 1, 13

---
+++ src/ch_11/home/lst_01/Triangle.java
@@ -0,0 +1,49 @@
+package ch_11.home.lst_01;
+
+import ch_11.lst_01_04.SimpleGeometricObject;
+
+public class Triangle extends SimpleGeometricObject {
+
+    private double side1;
+    private double side2;
+    private double side3;
+
+    public Triangle() {
+        side1 = 1;
+        side2 = 1;
+        side3 = 1;
+    }
+
+    public Triangle(double side1, double side2, double side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    public double getSide1() {
+        return side1;
+    }
+
+    public double getSide2() {
+        return side2;
+    }
+
+    public double getSide3() {
+        return side3;
+    }
+
+    public double getArea() {
+        double p = (side1 + side2 + side3) / 2;
+
+        return Math.sqrt(p * (p - side1) * (p - side2) * (p - side3));
+    }
+
+    public double getPerimeter() {
+        return side1 + side2 + side3;
+    }
+
+    @Override
+    public String toString() {
+        return "Triangle: side1 = " + side1 + " side2 = " + side2 + " side3 = " + side3;
+    }
+}
---
+++ src/ch_11/home/lst_13/RemoveDuplicates.java
@@ -0,0 +1,36 @@
+package ch_11.home.lst_13;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter ten integers: ");
+
+        ArrayList<Integer> list = new ArrayList<>(10);
+
+        for (int i = 0; i < 10; i++) {
+            list.add(input.nextInt());
+        }
+
+        removeDuplicate(list);
+
+        System.out.print("The distinct integers are ");
+
+        for (Integer i : list) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
+    public static void removeDuplicate(ArrayList<Integer> list) {
+        for (int i = 0; i < list.size() - 1; i++)
+            for (int j = i + 1; j < list.size(); j++)
+                if (list.get(j) == list.get(i))
+                    list.remove(j);
+    }
+
+}
